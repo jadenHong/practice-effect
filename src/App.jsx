119a9f7686b51d3e6074ad5caf450bfc2237f2d8
jadenHong <62584142+jadenHong@users.noreply.github.com>
2020-07-30 02:29:38 -0600

    React: useEffect and poketmon
    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 import React, { useState, useEffect } from "react";
 
+const Search = ({ search, change, click }) => {
+  return (
+    <div>
+      <input type="text" value={search} onChange={change} />
+      <button onClick={click}>Search</button>
+    </div>
+  )
+}
+
 const PokemonInfo = ({ name, exp, height }) => {
   return (
     <div>
@@ -11,28 +20,39 @@ const PokemonInfo = ({ name, exp, height }) => {
 };
 
 function App() {
-  const URL = "https://pokeapi.co/api/v2/pokemon/pikachu";
+  const PAGE = "https://pokeapi.co/api/v2/pokemon/";
+  const [search, setSearch] = useState("");
+  const URL = PAGE + search;
   const [name, setName] = useState("");
   const [exp, setExp] = useState(0);
   const [height, setHeight] = useState(0);
 
-  useEffect(() => {
-    getData();
-  }, []);
-
+  //pikachu
   const getData = () => {
+    // console.log('fetch');
     fetch(URL)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
+        // console.log(data);
         setName(data.name);
         setExp(data.base_experience);
         setHeight(data.height);
       });
   };
 
+  const handleChange = (e) => {
+    const newSearch = e.target.value;
+    setSearch(newSearch);
+    // console.log(newSearch);
+  }
+
+  const handleClick = () => {
+    getData();
+  }
+
   return (
     <div className="App">
+      <Search search={search} change={handleChange} click={handleClick} />
       <PokemonInfo name={name} exp={exp} height={height} />
     </div>
   );
